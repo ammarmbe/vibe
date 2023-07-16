55eb936d8cce5e0a0cd943912cb54c9721db41da
Ammar Elbehery <ammarelbehery04@gmail.com>
2023-07-16 08:15:15 +0300

    Added webhook

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,6 +1,8 @@
 import { db } from "@/lib/db";
 import { User } from "@/lib/types";
+import { Webhook } from "@clerk/backend/dist/types/api/resources/Webhooks";
 import { auth } from "@clerk/nextjs";
+import { UserJSON } from "@clerk/nextjs/dist/types/server";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -18,4 +20,18 @@ export async function GET(request: Request) {
   }
 
   return new Response(JSON.stringify(user));
+}
+
+export async function POST(request: Request) {
+  const body = (await request.json()) as Webhook<"user.created", UserJSON>;
+
+  await db.execute(
+    "INSERT INTO `users` (id, name, email, image) VALUES (:id, :name, :email, :image)",
+    {
+      id: body.data.id,
+      name: body.data.first_name + " " + body.data.last_name,
+      email: body.data.email_addresses[0].email_address,
+      image: body.data.image_url,
+    }
+  );
 }
