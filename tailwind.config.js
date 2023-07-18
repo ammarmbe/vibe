7c1b790565142fd2664ba8322136f7ea09f90031
Ammar Elbehery <ammarelbehery04@gmail.com>
2023-07-18 05:39:28 +0300

    Changed dark more colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,11 @@ module.exports = {
     },
     extend: {
       colors: {
-        main: "#cd002b",
-        mainDark: "#ad0528",
-        redBorder: "hsl(0 100% 89.8%)",
-        redRing: "hsl(0 100% 63.9%)",
-        danger: "#e63913",
+        main: "hsl(var(--main))",
+        mainDark: "hsl(var(--main-dark))",
+        redBorder: "hsl(var(--red-border))",
+        redRing: "hsl(var(--red-ring))",
+        danger: "hsl(var(--danger))",
 
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
