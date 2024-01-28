453ceb3d33f3831857c962c8c826c200a531104f
Ammar Elbehery <ammarelbehery04@gmail.com>
2024-01-28 04:32:14 +0200

    updated og image

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -18,7 +18,7 @@ export default async function Image() {
 	return new ImageResponse(
 		<div
 			style={{
-				fontSize: 278,
+				fontSize: 428,
 				background: "transparent",
 				width: "100%",
 				height: "100%",
