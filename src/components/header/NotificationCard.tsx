97e6690fb6d1ffae4e2ce5965037a2b4e911c577
Ammar Elbehery <ammarelbehery04@gmail.com>
2024-02-01 07:44:34 +0200

    updated notification card

--- src/components/header/NotificationCard.tsx
+++ src/components/header/NotificationCard.tsx
@@ -63,8 +63,6 @@ export default function NotificationCard({
 					{notification.notifierName}
 				</Link>{" "}
 				{(() => {
-					console.log(notification);
-
 					if (notification.type === "followedUser") {
 						return "followed you";
 					}
