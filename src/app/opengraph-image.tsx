6d2a0ce813f544b78252e435e885589029d5ff04
Ammar Elbehery <ammarelbehery04@gmail.com>
2024-10-02 12:40:37 +0300

    updated opengraph image

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -2,7 +2,7 @@ import { ImageResponse } from "next/og";
 
 export const runtime = "edge";
 
-export const alt = "About Acme";
+export const alt = "Vibe";
 export const size = {
   width: 1200,
   height: 630,
